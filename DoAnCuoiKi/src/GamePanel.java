7c5928452ec10e4748c26177968fb5d11b7d16f7
Nguyen Hoang Phuc <22730090@ms.uit.edu.vn>
2023-08-13 14:17:32 +0700

    add draw
    snake drawing

--- DoAnCuoiKi/src/GamePanel.java
+++ DoAnCuoiKi/src/GamePanel.java
@@ -5,8 +5,6 @@ import javax.swing.*;
 
 import java.util.Random;
 
-import javax.swing.JPanel;
-
 public class GamePanel extends JPanel implements ActionListener {
 
     static final int SCREEN_WIDTH = 600;
@@ -42,8 +40,38 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		draw(g);
-	}
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g) {
+
+        if (running) {
+            // for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
+            // g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
+            // g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
+            // }
+            g.setColor(Color.red);
+            g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+
+            for (int i = 0; i < bodyParts; i++) {
+                if (i == 0) {
+                    g.setColor(Color.green);
+                    g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+                } else {
+                    // g.setColor(new Color(45,180,0));
+                    g.setColor(new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255)));
+                    g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+                }
+            }
+            g.setColor(Color.orange);
+            g.setFont(new Font("Ink Free", Font.BOLD, 40));
+            FontMetrics metrics = getFontMetrics(g.getFont());
+            g.drawString("Score: " + appleEaten, (SCREEN_WIDTH - metrics.stringWidth("Score: " + appleEaten)) / 2,
+                    g.getFont().getSize());
+        } else {
+            gameOver(g);
+        }
+    }
 
 }
